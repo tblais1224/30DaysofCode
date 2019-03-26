1c1b5bf7163e4dd57732e6fa0cef76729237f653
tblais1224 <thomasblais.student@careerdevs.com>
2019-03-26 08:54:04 -0400

    merge day11

---
+++ day11.js
@@ -0,0 +1,5 @@
+// day 11: 2d arrays
+// Calculate the hourglass sum for every hourglass in A, then print the maximum hourglass sum.
+//Input Format
+//There are 6 lines of input, where each line contains 6 space-separated integers describing 2D Array A; every value in A will be in the inclusive range of  to .
+

aed2c8888742cc45ad374b1e8db91c37d4adb7dc
tblais1224 <thomasblais.student@careerdevs.com>
2019-04-17 16:53:04 -0400

    added day14 in c#

---
+++ 30days/day14/Program.cs
@@ -0,0 +1,44 @@
+﻿// Complete the Difference class by writing the following:
+
+// A class constructor that takes an array of integers as a parameter and saves it to the  instance variable.
+// A computeDifference method that finds the maximum absolute difference between any numbers in  and stores it in the  instance variable.
+
+using System;
+using System.Linq;
+
+namespace day14 {
+    class Difference {
+        private int[] elements;
+        public int maximumDifference;
+
+        public Difference (int[] a) {
+            elements = a;
+        }
+        public void computeDifference () {
+            int max = elements[0];
+            int min = elements[0];
+            for (int i = 0; i < elements.Length; i++) {
+                if (elements[i] < min) {
+                    min = elements[i];
+                } else if (elements[i] > max) {
+                    max = elements[i];
+                }
+            }
+            maximumDifference = max - min;
+        }
+    } // End of Difference Class
+
+    class Solution {
+        static void Main (string[] args) {
+            Convert.ToInt32 (Console.ReadLine ());
+
+            int[] a = Console.ReadLine ().Split (' ').Select (x => Convert.ToInt32 (x)).ToArray ();
+
+            Difference d = new Difference (a);
+
+            d.computeDifference ();
+
+            Console.Write (d.maximumDifference);
+        }
+    }
+}
--- 30days/day29.js
+++ 30days/day29.js
@@ -51,3 +51,4 @@ function main() {
         console.log(output);
     }
 }
+

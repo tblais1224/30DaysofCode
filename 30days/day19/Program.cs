16c6ce03d307e33f12cdb3987be63a54e75a378d
tblais1224 <thomasblais.student@careerdevs.com>
2019-04-17 17:30:57 -0400

    day19 added

---
+++ 30days/day19/Program.cs
@@ -0,0 +1,35 @@
+﻿//interfaces
+//The AdvancedArithmetic interface and the method declaration for the abstract divisorSum(n) method are provided for you in the editor below.
+
+//Complete the implementation of Calculator class, which implements the AdvancedArithmetic interface. The implementation for the divisorSum(n) method must return the sum of all divisors of n.
+
+using System;
+
+namespace day19 {
+    public interface AdvancedArithmetic {
+        int divisorSum (int n);
+    }
+
+    public class Calculator : AdvancedArithmetic {
+        public int divisorSum (int n) {
+            int output = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    output += i;
+                }
+            }
+            return output;
+        }
+    }
+
+    class Solution {
+        static void Main (string[] args) {
+            int n = Int32.Parse (Console.ReadLine ());
+            AdvancedArithmetic myCalculator = new Calculator ();
+            int sum = myCalculator.divisorSum (n);
+            Console.WriteLine ("I implemented: AdvancedArithmetic\n" + sum);
+        }
+    }
+}

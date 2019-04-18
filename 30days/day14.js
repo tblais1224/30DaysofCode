3caab000cf229491c6f6fd9ed7c33cb112de7358
tblais1224 <thomasblais.student@careerdevs.com>
2019-04-17 22:00:14 -0400

    completed all 30 days

---
+++ 30days/day14.js
@@ -0,0 +1,7 @@
+//Scope
+
+//Complete the Difference class by writing the following:
+
+// A class constructor that takes an array of integers as a parameter and saves it to the elements instance variable.
+// A computeDifference method that finds the maximum absolute difference between any 2 numbers in N and stores it in the maximumDifference instance variable.
+
